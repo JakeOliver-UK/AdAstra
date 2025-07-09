4e5859e26428916558058fed2b651d5baaf361c1
JakeOliver-UK <jake_oliver@live.co.uk>
2025-07-09 19:42:25 +0100

    Implement collider component.

---
+++ Engine/Entities/Components/Collider.cs
@@ -0,0 +1,83 @@
+﻿using AdAstra.Engine.Drawing;
+using AdAstra.Engine.Extensions;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace AdAstra.Engine.Entities.Components
+{
+    internal class Collider : Component
+    {
+        public int Sides { get => _sides; set => _sides = Math.Max(3, value); }
+        public float Radius { get => _radius; set => _radius = MathF.Max(0.0f, value); }
+        public Polygon Bounds => _bounds;
+        public bool DrawBounds { get; set; } = false;
+        public int BoundsLayer { get; set; } = 0;
+
+        private Polygon _bounds;
+        private int _sides;
+        private float _radius;
+        private readonly List<Collider> _collisions;
+
+        public delegate void CollisionEventHandler(Collider other);
+
+        public event CollisionEventHandler OnCollisionEnter;
+        public event CollisionEventHandler OnCollisionStay;
+        public event CollisionEventHandler OnCollisionExit;
+
+        public override void Update()
+        {
+            base.Update();
+
+            UpdateBounds();
+            CheckCollisions();
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+
+            if (DrawBounds) App.Instance.SpriteBatch.DrawPolygon(_bounds, Color.Lime, 1.0f, BoundsLayer);
+        }
+
+        private void UpdateBounds()
+        {
+            _bounds = new(Entity.Transform.Position, _sides, _radius, Entity.Transform.Rotation);
+        }
+
+        private void CheckCollisions()
+        {
+            Entity[] entities = Entity.Manager.GetWithComponent<Collider>();
+
+            for (int i = 0; i < entities.Length; i++)
+            {
+                if (entities[i] == null || entities[i] == Entity || !entities[i].IsActive) continue;
+                if (entities[i].GetComponent<Collider>() == null) continue;
+                if (entities[i].GetComponent<Collider>() == this) continue;
+                Collider otherCollider = entities[i].GetComponent<Collider>();
+                if (otherCollider == null || otherCollider == this || !otherCollider.Entity.IsActive) continue;
+                
+                if (_bounds.Intersects(otherCollider._bounds))
+                {
+                    if (!_collisions.Contains(otherCollider))
+                    {
+                        _collisions.Add(otherCollider);
+                        OnCollisionEnter?.Invoke(otherCollider);
+                    }
+                    else
+                    {
+                        OnCollisionStay?.Invoke(otherCollider);
+                    }
+                }
+                else
+                {
+                    if (_collisions.Contains(otherCollider))
+                    {
+                        OnCollisionExit?.Invoke(otherCollider);
+                        _collisions.Remove(otherCollider);
+                    }
+                }
+            }
+        }
+    }
+}
--- Scenes/MainScene.cs
+++ Scenes/MainScene.cs
@@ -12,10 +12,6 @@ namespace AdAstra.Scenes
     internal class MainScene : Scene
     {
         private Entity _player;
-        private Polygon _polygon;
-        private Color _polygonColor = Color.Lime;
-        private Polygon _polygon2;
-        private Color _polygon2Color = Color.Cyan;
 
         public override void Initialize()
         {
@@ -26,8 +22,6 @@ namespace AdAstra.Scenes
             _player.Transform.Position = new(100.0f);
             _player.AddComponent<ImageRenderer>().Image = "playerShip1_blue";
             _player.GetComponent<ImageRenderer>().Scale = new(0.5f);
-            _polygon = new(new Vector2(200.0f), 5, 32.0f, 0.0f);
-            _polygon2 = new(InputManager.MousePosition, 3, 16.0f, MathHelper.ToRadians(45.0f));
         }
 
         public override void Update()
@@ -37,27 +31,6 @@ namespace AdAstra.Scenes
             if (InputManager.IsKeyDown(Keys.W)) _player.Transform.Position += _player.Transform.Forward * 100.0f * Time.Delta;
             if (InputManager.IsKeyDown(Keys.A)) _player.Transform.Rotation -= 1.0f * Time.Delta;
             if (InputManager.IsKeyDown(Keys.D)) _player.Transform.Rotation += 1.0f * Time.Delta;
-
-            _polygon2.Position = InputManager.MousePosition;
-
-            if (_polygon.Intersects(_polygon2))
-            {
-                _polygonColor = Color.Red;
-                _polygon2Color = Color.Yellow;
-            }
-            else
-            {
-                _polygonColor = Color.Lime;
-                _polygon2Color = Color.Cyan;
-            }
-        }
-
-        public override void DrawWorld()
-        {
-            base.DrawWorld();
-
-            App.Instance.SpriteBatch.DrawPolygon(_polygon, _polygonColor, 1.0f, 1);
-            App.Instance.SpriteBatch.DrawPolygon(_polygon2, _polygon2Color, 1.0f, 1);
         }
     }
 }
