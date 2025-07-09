5e58532670392866076e184d71261a264d8743f3
JakeOliver-UK <jake_oliver@live.co.uk>
2025-07-09 22:33:26 +0100

    Add rigidbody component.

---
+++ Engine/Entities/Components/Rigidbody.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace AdAstra.Engine.Entities.Components
+{
+    internal class Rigidbody : Component
+    {
+        public Vector2 Velocity => _velocity;
+        public float AngularVelocity => _angularVelocity;
+        public float LinearDrag { get; set; } = 0.5f;
+        public float AngularDrag { get; set; } = 0.05f;
+        public float Mass { get; set; } = 1.0f;
+        public float MaxSpeed { get; set; } = 100.0f;
+        public float MaxAngularSpeed { get; set; } = 1.0f;
+
+        private Vector2 _velocity;
+        private float _angularVelocity;
+        private Vector2 _forceAccumulator;
+        private float _torqueAccumulator;
+
+        public override void Update()
+        {
+            base.Update();
+
+            _velocity += (_forceAccumulator / Mass) * Time.Delta;
+            _forceAccumulator = Vector2.Zero;
+            float speedSq = _velocity.LengthSquared();
+            float maxSpeedSq = MaxSpeed * MaxSpeed;
+            if (speedSq > maxSpeedSq && maxSpeedSq > 0f) _velocity = Vector2.Normalize(_velocity) * MaxSpeed;
+            float linearDragFactor = MathF.Pow(LinearDrag, Time.Delta);
+            _velocity *= linearDragFactor;
+            Entity.Transform.Position += _velocity * Time.Delta;
+            
+            _angularVelocity += (_torqueAccumulator / Mass) * Time.Delta;
+            _torqueAccumulator = 0f;
+            if (MathF.Abs(_angularVelocity) > MaxAngularSpeed && MaxAngularSpeed > 0f) _angularVelocity = MathF.Sign(_angularVelocity) * MaxAngularSpeed;
+            float angularDragFactor = MathF.Pow(AngularDrag, Time.Delta);
+            _angularVelocity *= angularDragFactor;
+            Entity.Transform.Rotation += _angularVelocity * Time.Delta;
+        }
+
+        public void AddForce(Vector2 force) => _forceAccumulator += force;
+        public void AddForce(Vector2 direction, float magnitude) => _forceAccumulator += Vector2.Normalize(direction) * magnitude;
+        public void AddForce(float magnitude) => _forceAccumulator += Entity.Transform.Forward * magnitude;
+        public void AddTorque(float torque) => _torqueAccumulator += torque;
+        public void SetVelocity(Vector2 velocity) => _velocity = velocity;
+        public void SetAngularVelocity(float angularVelocity) => _angularVelocity = angularVelocity;
+    }
+}
+
--- Scenes/MainScene.cs
+++ Scenes/MainScene.cs
@@ -1,8 +1,6 @@
 ﻿using AdAstra.Engine;
-using AdAstra.Engine.Drawing;
 using AdAstra.Engine.Entities;
 using AdAstra.Engine.Entities.Components;
-using AdAstra.Engine.Extensions;
 using AdAstra.Engine.Managers.Global;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
@@ -24,27 +22,16 @@ namespace AdAstra.Scenes
             _player.Transform.Position = new(100.0f);
             _player.AddComponent<ImageRenderer>().Image = "playerShip1_blue";
             _player.GetComponent<ImageRenderer>().Scale = new(0.5f);
-            _player.AddComponent<Collider>().DrawBounds = true;
-            _player.GetComponent<Collider>().Sides = 100;
+            _player.AddComponent<Collider>().Sides = 100;
             _player.GetComponent<Collider>().Width = 64.0f;
             _player.GetComponent<Collider>().Height = 48.0f;
-            _player.GetComponent<Collider>().OnCollisionEnter += (other) =>
-            {
-                _player.GetComponent<Collider>().BoundsColor = Color.Red;
-                other.BoundsColor = Color.Red;
-            };
-            _player.GetComponent<Collider>().OnCollisionExit += (other) =>
-            {
-                _player.GetComponent<Collider>().BoundsColor = Color.Lime;
-                other.BoundsColor = Color.Lime;
-            };
+            _player.AddComponent<Rigidbody>();
 
             _other = WorldEntityManager.Create("Other");
             _other.Transform.Position = new(200.0f);
             _other.AddComponent<ImageRenderer>().Image = "playerShip2_red";
             _other.GetComponent<ImageRenderer>().Scale = new(0.5f);
-            _other.AddComponent<Collider>().DrawBounds = true;
-            _other.GetComponent<Collider>().Sides = 100;
+            _other.AddComponent<Collider>().Sides = 100;
             _other.GetComponent<Collider>().Width = 64.0f;
             _other.GetComponent<Collider>().Height = 48.0f;
         }
@@ -53,9 +40,9 @@ namespace AdAstra.Scenes
         {
             base.Update();
 
-            if (InputManager.IsKeyDown(Keys.W)) _player.Transform.Position += _player.Transform.Forward * 100.0f * Time.Delta;
-            if (InputManager.IsKeyDown(Keys.A)) _player.Transform.Rotation -= 1.0f * Time.Delta;
-            if (InputManager.IsKeyDown(Keys.D)) _player.Transform.Rotation += 1.0f * Time.Delta;
+            if (InputManager.IsKeyDown(Keys.W)) _player.GetComponent<Rigidbody>().AddForce(100.0f);
+            if (InputManager.IsKeyDown(Keys.A)) _player.GetComponent<Rigidbody>().AddTorque(-5.0f);
+            if (InputManager.IsKeyDown(Keys.D)) _player.GetComponent<Rigidbody>().AddTorque(5.0f);
         }
     }
 }
